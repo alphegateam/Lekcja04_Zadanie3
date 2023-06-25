13a2681b7efdca46402240661cf39d28f1380657
manx <manx@DESKTOP-95I9G38>
2023-06-25 21:56:16 +0200

    Add project files.

---
+++ Lekcja04_Zadanie3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Zadanie_4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lekcja 4, zadanie dowowe 3.
+            // Przekątna prostokąta = √(a² + b²)
+            string a, b;
+            int boka, bokb;
+            double wynik;
+
+            Console.WriteLine("Podaj długość kobu a: ");
+            a = Console.ReadLine();
+            Console.WriteLine("\n Podaj długość boku b: ");
+            b = Console.ReadLine();
+            Int32.TryParse(a, out boka);
+            Int32.TryParse(b, out bokb);
+            wynik = Math.Sqrt(Math.Pow(boka, 2) + Math.Pow(bokb, 2));
+
+            Console.WriteLine($"\n Oto długość przekątnej podanego prostokąta: {wynik}");
+ 
+        }
+    }
+}
+
+
